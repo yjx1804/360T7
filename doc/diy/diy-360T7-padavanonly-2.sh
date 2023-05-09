30499b718e7137cb46d051afa05e582dfed04758
yjx1804 <73804454+yjx1804@users.noreply.github.com>
2023-05-09 15:58:57 +0800

    Update diy-360T7-padavanonly-2.sh
    Signed-off-by: yjx1804 <73804454+yjx1804@users.noreply.github.com>

--- doc/diy/diy-360T7-padavanonly-2.sh
+++ doc/diy/diy-360T7-padavanonly-2.sh
@@ -19,8 +19,8 @@ sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Themes
-#git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
@@ -31,27 +31,27 @@ sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 
 # 科学上网插件
-# svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
-# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+ svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
+ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+ svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+ svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 
 # 科学上网插件依赖
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
+ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
 # svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
 # svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
 # svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
